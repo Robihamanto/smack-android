e065512a820a4896f3fe23624e831a6a63a23ba9
Robihamanto <rrobie44@gmail.com>
2019-05-23 09:45:20 +0800

    added login view

---
+++ app/src/main/java/com/learn/smackandroid/LoginActivity.kt
@@ -0,0 +1,23 @@
+package com.learn.smackandroid
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+    fun loginButtonDidTap(view: View) {
+
+    }
+
+    fun signUpButtonDidTap(view: View) {
+
+    }
+
+
+}
--- app/src/main/java/com/learn/smackandroid/MainActivity.kt
+++ app/src/main/java/com/learn/smackandroid/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.learn.smackandroid
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.FloatingActionButton
 import android.support.design.widget.Snackbar
@@ -41,7 +42,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun loginButtonDidTap(view: View) {
-
+        val loginIntent = Intent(this, LoginActivity::class.java)
+        startActivity(loginIntent)
     }
 
     fun addChannelButtonDidTap(view: View) {
