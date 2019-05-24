80466da26c864960ca442c50b154bcb81f6b1ebf
Robihamanto <rrobie44@gmail.com>
2019-05-24 10:52:59 +0800

    added signup handler and broadcast

--- app/src/main/java/com/learn/smackandroid/Controller/SignUpActivity.kt
+++ app/src/main/java/com/learn/smackandroid/Controller/SignUpActivity.kt
@@ -1,14 +1,18 @@
 package com.learn.smackandroid.Controller
 
+import android.content.Intent
 import android.graphics.Color
+import android.opengl.Visibility
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.content.LocalBroadcastManager
 import android.util.Log
 import android.view.View
 import android.widget.Toast
 import com.learn.smackandroid.R
 import com.learn.smackandroid.Services.AuthService
 import com.learn.smackandroid.Services.UserDataService
+import com.learn.smackandroid.Utilities.BROADCAST_USER_DATA_CHANGE
 import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_sign_up.*
 import java.util.*
@@ -18,6 +22,7 @@ class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+        signUpSpinner.visibility = View.INVISIBLE
     }
 
     var userAvatar = "light0"
@@ -56,35 +61,63 @@ class SignUpActivity : AppCompatActivity() {
     }
 
     fun signUpSignUpButtonDidTap(view: View) {
+        enableSpinner(true)
         val username = usernameTextField.text.toString()
         val email = emailTextField.text.toString()
         val password = passwordTextField.text.toString()
 
-        AuthService.registerUser(this, username, email, password) { success ->
-            if (success) {
-                Log.d("Register", "Register Successful")
-                AuthService.login(this, email, password) { success ->
-                    if (success) {
-                        Log.d("Login", "Login Successful")
-                        AuthService.createUser(this, username, email, userAvatar, avatarColor) { success ->
-                            if (success) {
-                                println(UserDataService.name)
-                                println(UserDataService.email)
-                                println(UserDataService.avatarName)
-                                println(UserDataService.avatarColor)
-                                finish()
+        if (username.isNotEmpty() && email.isNotEmpty() && password.isNotEmpty()) {
+            AuthService.registerUser(this, username, email, password) { success ->
+                if (success) {
+                    Log.d("Register", "Register Successful")
+                    AuthService.login(this, email, password) { success ->
+                        if (success) {
+                            Log.d("Login", "Login Successful")
+                            AuthService.createUser(this, username, email, userAvatar, avatarColor) { success ->
+                                if (success) {
+                                    broadCastUserDataChange()
+                                    enableSpinner(false)
+                                    finish()
+                                } else {
+                                    errorToast()
+                                }
                             }
+                        } else {
+                            Log.d("Login", "Login Error, Something Happened")
+                            errorToast()
                         }
-                    } else {
-                        Log.d("Login", "Login Error, Something Happened")
                     }
+                } else {
+                    Log.d("Register", "Register Error, Something Happened")
+                    errorToast()
                 }
-            } else {
-                Log.d("Register", "Register Error, Something Happened")
             }
+        } else {
+            Toast.makeText(this, "Please fill all field.", Toast.LENGTH_SHORT).show()
+            enableSpinner(false)
         }
-
     }
 
+    fun broadCastUserDataChange() {
+        val userDataChange = Intent(BROADCAST_USER_DATA_CHANGE)
+        LocalBroadcastManager.getInstance(this).sendBroadcast(userDataChange)
+    }
+
+    fun enableSpinner(enable: Boolean) {
+        if (enable) {
+            signUpSpinner.visibility = View.VISIBLE
+        } else {
+            signUpSpinner.visibility = View.INVISIBLE
+        }
+        signUpLoginButton.isEnabled = !enable
+        generateBackgroundButton.isEnabled = !enable
+        avatarImageView.isEnabled = !enable
+    }
+
+
+    fun errorToast() {
+        Toast.makeText(this, "Something wrong, Please try again.", Toast.LENGTH_SHORT).show()
+        enableSpinner(false)
+    }
 
 }
--- app/src/main/java/com/learn/smackandroid/Utilities/Constants.kt
+++ app/src/main/java/com/learn/smackandroid/Utilities/Constants.kt
@@ -1,6 +1,8 @@
 package com.learn.smackandroid.Utilities
 
-val BASE_URL = "https://talkischat.herokuapp.com/v1"
-val URL_REGISTER = "$BASE_URL/account/register"
-val URL_LOGIN = "$BASE_URL/account/login"
-val URL_CREATE_USER = "$BASE_URL/user/add"
+const val BASE_URL = "https://talkischat.herokuapp.com/v1"
+const val URL_REGISTER = "$BASE_URL/account/register"
+const val URL_LOGIN = "$BASE_URL/account/login"
+const val URL_CREATE_USER = "$BASE_URL/user/add"
+
+const val BROADCAST_USER_DATA_CHANGE = "BROADCAST_USER_DATA_CHANGE"
